1e77147025a8500ea81428084d6c3766f5e8d0f0
WYT_IN_TTI <as5818956@gmail.com>
2025-04-07 18:12:35 +0800

    update

---
+++ 01-Fundamentals-Part-1/starter/scripts.js
@@ -0,0 +1,12 @@
+// let js = "amazing";
+// if (js === "amazing") alert("JavaScript is FUN! 😍");
+// console.log(40 + 8 + 23 - 110);
+
+const firstName = "Jonas";
+
+const jonos = `I'm ${firstName}, a teacher and a student!`;
+console.log(jonos); 
+
+console.log(`String
+    with
+    multiple lines`);
