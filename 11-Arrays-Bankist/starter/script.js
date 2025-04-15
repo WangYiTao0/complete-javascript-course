470a97e1a525b2490967d40d778ae503fa283e39
WYT_IN_TTI <as5818956@gmail.com>
2025-04-15 17:37:11 +0800

    Update

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -76,7 +76,9 @@ const UpdateUI = function (acc) {
 const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = ''; // Clear the container
 
-  movements.forEach((mov, i) => {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements; // Sort the movements if needed
+
+  movs.forEach((mov, i) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
       <div class="movements__row">
@@ -131,7 +133,7 @@ const createUsernames = function (accs) {
 
 createUsernames(accounts);
 
-console.log(accounts);
+//console.log(accounts);
 
 let currentAccount = null; // Variable to store the current account
 //Event Handler
@@ -212,10 +214,12 @@ btnLoan.addEventListener('click', function (e) {
   inputLoanAmount.value = '';
 });
 
+let sorted = false; // Variable to track the sorting state
 btnSort.addEventListener('click', function (e) {
   e.preventDefault(); // Prevent form from submitting
-  const sorted = containerMovements.classList.toggle('sort');
+  //const sorted = containerMovements.classList.toggle('sort');
   displayMovements(currentAccount.movements, sorted);
+  sorted = !sorted; // Toggle the sorting state
 });
 
 /////////////////////////////////////////////////
@@ -396,101 +400,226 @@ BONUS: What's the average weight of the heaviest breed that likes to fetch? HINT
 TEST DATA:
 */
 
-const breeds = [
-  {
-    breed: 'German Shepherd',
-    averageWeight: 32,
-    activities: ['fetch', 'swimming'],
-  },
-  {
-    breed: 'Dalmatian',
-    averageWeight: 24,
-    activities: ['running', 'fetch', 'agility'],
-  },
-  {
-    breed: 'Labrador',
-    averageWeight: 28,
-    activities: ['swimming', 'fetch'],
-  },
-  {
-    breed: 'Beagle',
-    averageWeight: 12,
-    activities: ['digging', 'fetch'],
-  },
-  {
-    breed: 'Husky',
-    averageWeight: 26,
-    activities: ['running', 'agility', 'swimming'],
-  },
-  {
-    breed: 'Bulldog',
-    averageWeight: 36,
-    activities: ['sleeping'],
-  },
-  {
-    breed: 'Poodle',
-    averageWeight: 18,
-    activities: ['agility', 'fetch'],
-  },
+// const breeds = [
+//   {
+//     breed: 'German Shepherd',
+//     averageWeight: 32,
+//     activities: ['fetch', 'swimming'],
+//   },
+//   {
+//     breed: 'Dalmatian',
+//     averageWeight: 24,
+//     activities: ['running', 'fetch', 'agility'],
+//   },
+//   {
+//     breed: 'Labrador',
+//     averageWeight: 28,
+//     activities: ['swimming', 'fetch'],
+//   },
+//   {
+//     breed: 'Beagle',
+//     averageWeight: 12,
+//     activities: ['digging', 'fetch'],
+//   },
+//   {
+//     breed: 'Husky',
+//     averageWeight: 26,
+//     activities: ['running', 'agility', 'swimming'],
+//   },
+//   {
+//     breed: 'Bulldog',
+//     averageWeight: 36,
+//     activities: ['sleeping'],
+//   },
+//   {
+//     breed: 'Poodle',
+//     averageWeight: 18,
+//     activities: ['agility', 'fetch'],
+//   },
+// ];
+
+// // 1. Store the the average weight of a "Husky" in a variable "huskyWeight"
+// const huskyWeight = breeds.find(breed => breed.breed === 'Husky').averageWeight;
+// console.log(huskyWeight); // 26
+// // 2. Find the name of the only breed that likes both "running" and "fetch"
+// // ("dogBothActivities" variable)
+
+// const dogBothActivities = breeds.find(
+//   breed =>
+//     breed.activities.includes('running') && breed.activities.includes('fetch')
+// ).breed;
+// console.log(dogBothActivities); // Dalmatian
+
+// // 3. Create an array "allActivities" of all the activities of all the dog breeds
+// const allActivities = breeds
+//   .map(breed => breed.activities)
+//   .flat()
+//   .reduce((acc, cur) => acc.concat(cur), []);
+// const allActivities2 = breeds.flatMap(breed => breed.activities);
+// console.log(allActivities); // ['fetch', 'swimming', 'running', 'fetch', 'agility', 'swimming', 'digging', 'fetch', 'running', 'agility', 'sleeping', 'agility', 'fetch']
+// console.log(allActivities2); // ['fetch', 'swimming', 'running', 'fetch', 'agility', 'swimming', 'fetch', 'digging', 'fetch', 'running', 'agility', 'swimming', 'sleeping', 'agility', 'fetch']
+
+// // 4. Create an array "uniqueActivities" that contains only the unique activities
+// //  (no activity repetitions). HINT: Use a technique with a special data structure
+// // that we studied a few sections ago.
+
+// const uniqueActivities = [...new Set(allActivities)];
+// console.log(uniqueActivities); // ['fetch', 'swimming', 'running', 'agility', 'digging', 'sleeping']
+
+// // 5. Many dog breeds like to swim. What other activities do these dogs like?
+// // Store all the OTHER activities these breeds like to do,
+// // in a unique array called "swimmingAdjacent".
+// //
+// const dogLikeSwiming = breeds
+//   .filter(breed => breed.activities.includes('swimming'))
+//   .flatMap(breed => breed.activities);
+
+// const swimmingAdjacent = [...new Set(dogLikeSwiming)].filter(
+//   activity => activity !== 'swimming'
+// );
+// console.log(swimmingAdjacent); // ['fetch', 'swimming', 'running', 'agility']
+
+// // 6. Do all the breeds have an average weight of 10kg or more?
+// // Log to the console whether "true" or "false".
+// const allBreedsHaveWeight = breeds.every(breed => breed.averageWeight >= 10);
+// console.log(allBreedsHaveWeight); // true
+// // 7. Are there any breeds that are "active"?
+// // "Active" means that the dog has 3 or more activities.
+// // Log to the console whether "true" or "false".
+// const anyActiveBreeds = breeds.some(breed => breed.activities.length >= 3);
+// console.log(anyActiveBreeds); // true
+
+// // BONUS: What's the average weight of the heaviest breed that likes to fetch?
+// // HINT: Use the "Math.max" method along with the ... operator.
+// const heaviestBreed = Math.max(
+//   ...breeds
+//     .filter(breed => breed.activities.includes('fetch'))
+//     .map(breed => breed.averageWeight)
+// );
+
+// console.log(heaviestBreed); // 36
+
+// const groupedMovement = Object.groupBy(movements, movement =>
+//   movement > 0 ? 'deposits' : 'withdrawals'
+// );
+
+// console.log(groupedMovement);
+
+// //group object, group by object one property
+// const groupedByActivity = Object.groupBy(accounts, account => {
+//   const movementCount = account.movements.length;
+//   if (movementCount >= 8) return 'very nice';
+//   if (movementCount >= 4) return 'active';
+//   if (movementCount >= 1) return 'moderate';
+//   return 'inactive';
+// });
+// console.log(groupedByActivity);
+
+// const x = new Array(7);
+// x.fill(1);
+// x.fill(2, 3, 5);
+// console.log(x);
+
+// // Array.from
+// const y = Array.from({ length: 7 }, (_, i) => i + 1);
+// console.log(y);
+
+// labelBalance,
+//   addEventListener('click', () => {
+//     const movementsUI = Array.from(
+//       document.querySelectorAll('.movements__value')
+//     );
+
+//     console.log(movementsUI.map(el => Number(el.textContent.replace('€', ''))));
+//   });
+
+// we dont want mutate origin arry
+
+// console.log(movements);
+// //const reversedMov = movements.slice().reverse();
+// const reversedMov = movements.toReversed();
+// console.log(movements);
+// console.log(reversedMov);
+
+// toSorted / toSpliced
+// const sortedMov = movements.toSorted((a, b) => a - b);
+// console.log(movements);
+// console.log(sortedMov);
+
+// const splicedMov = movements.toSpliced(2, 3);
+// console.log(movements);
+// console.log(splicedMov);
+
+///////////////////////////////////////
+// Coding Challenge #5
+
+/* 
+Julia and Kate are still studying dogs. This time they are want to figure out if the dogs in their are eating too much or too little food.
+
+- Formula for calculating recommended food portion: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+- Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+- Eating an okay amount means the dog's current food portion is within a range 10% above and below the recommended portion (see hint).
+
+YOUR TASKS:
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion (recFood)
+   add it to the object as a new property. Do NOT create a new array, simply loop over the array (We never did this before,
+  so think about how you can do this without creating a new array).
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple users,
+ so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+3. Create an array containing all owners of dogs who eat too much (ownersTooMuch) and an array with all owners of
+ dogs who eat too little (ownersTooLittle).
+4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!"
+ and "Sarah and John and Michael's dogs eat too little!"
+5. Log to the console whether there is ANY dog eating EXACTLY the amount of food that is recommended (just true or false)
+6. Log to the console whether ALL of the dogs are eating an OKAY amount of food (just true or false)
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+8. Group the dogs into the following 3 groups: 'exact', 'too-much' and 'too-little', based on whether they are eating too much, 
+too little or the exact amount of food, based on the recommended food portion.
+9. Group the dogs by the number of owners they have
+10. Sort the dogs array by recommended food portion in an ascending order. Make sure to NOT mutate the original array!
+
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10).
+ Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John', 'Leo'] },
+  { weight: 18, curFood: 244, owners: ['Joe'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
 ];
 
-// 1. Store the the average weight of a "Husky" in a variable "huskyWeight"
-const huskyWeight = breeds.find(breed => breed.breed === 'Husky').averageWeight;
-console.log(huskyWeight); // 26
-// 2. Find the name of the only breed that likes both "running" and "fetch"
-// ("dogBothActivities" variable)
+GOOD LUCK 😀
+*/
 
-const dogBothActivities = breeds.find(
-  breed =>
-    breed.activities.includes('running') && breed.activities.includes('fetch')
-).breed;
-console.log(dogBothActivities); // Dalmatian
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John', 'Leo'] },
+  { weight: 18, curFood: 244, owners: ['Joe'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
 
-// 3. Create an array "allActivities" of all the activities of all the dog breeds
-const allActivities = breeds
-  .map(breed => breed.activities)
-  .flat()
-  .reduce((acc, cur) => acc.concat(cur), []);
-const allActivities2 = breeds.flatMap(breed => breed.activities);
-console.log(allActivities); // ['fetch', 'swimming', 'running', 'fetch', 'agility', 'swimming', 'digging', 'fetch', 'running', 'agility', 'sleeping', 'agility', 'fetch']
-console.log(allActivities2); // ['fetch', 'swimming', 'running', 'fetch', 'agility', 'swimming', 'fetch', 'digging', 'fetch', 'running', 'agility', 'swimming', 'sleeping', 'agility', 'fetch']
+//1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion (recFood)
+// and add it to the object as a new property. Do NOT create a new array, simply loop over the array (We never did this before,
 
-// 4. Create an array "uniqueActivities" that contains only the unique activities
-//  (no activity repetitions). HINT: Use a technique with a special data structure
-// that we studied a few sections ago.
+dogs.forEach(dog => {
+  dog.recFood = Math.trunc(dog.weight ** 0.75 * 28);
+});
+console.log(dogs);
 
-const uniqueActivities = [...new Set(allActivities)];
-console.log(uniqueActivities); // ['fetch', 'swimming', 'running', 'agility', 'digging', 'sleeping']
-
-// 5. Many dog breeds like to swim. What other activities do these dogs like?
-// Store all the OTHER activities these breeds like to do,
-// in a unique array called "swimmingAdjacent".
-//
-const dogLikeSwiming = breeds
-  .filter(breed => breed.activities.includes('swimming'))
-  .flatMap(breed => breed.activities);
-
-const swimmingAdjacent = [...new Set(dogLikeSwiming)].filter(
-  activity => activity !== 'swimming'
-);
-console.log(swimmingAdjacent); // ['fetch', 'swimming', 'running', 'agility']
-
-// 6. Do all the breeds have an average weight of 10kg or more?
-// Log to the console whether "true" or "false".
-const allBreedsHaveWeight = breeds.every(breed => breed.averageWeight >= 10);
-console.log(allBreedsHaveWeight); // true
-// 7. Are there any breeds that are "active"?
-// "Active" means that the dog has 3 or more activities.
-// Log to the console whether "true" or "false".
-const anyActiveBreeds = breeds.some(breed => breed.activities.length >= 3);
-console.log(anyActiveBreeds); // true
-
-// BONUS: What's the average weight of the heaviest breed that likes to fetch?
-// HINT: Use the "Math.max" method along with the ... operator.
-const heaviestBreed = Math.max(
-  ...breeds
-    .filter(breed => breed.activities.includes('fetch'))
-    .map(breed => breed.averageWeight)
-);
-
-console.log(heaviestBreed); // 36
+//2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple users,
+// so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+const sarahDog = dogs
+  .filter(dog => dog.owners.includes('Sarah'))
+  .forEach(dog => {
+    if (dog.curFood > dog.recFood) {
+      console.log(`Sarah's dog is eating too much`);
+    } else if (dog.curFood < dog.recFood) {
+      console.log(`Sarah's dog is eating too little`);
+    } else {
+      console.log(`Sarah's dog is eating the exact amount of food`);
+    }
+  });
--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -4,11 +4,16 @@
 // Modal window
 
 const modal = document.querySelector('.modal');
+const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const btnScollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+const openModal = function (e) {
+  e.preventDefault(); // Prevent default action of the button
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -17,6 +22,10 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+btnsOpenModal.forEach(btn => {
+  btn.addEventListener('click', openModal);
+});
 
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
@@ -29,3 +38,285 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+//page navigation
+// const navLinks = document.querySelectorAll('.nav__link');
+// console.log(navLinks); // log the nav links
+// navLinks.forEach(el => {
+//   el.addEventListener('click', e => {
+//     e.preventDefault(); // Prevent default action of the link
+//     const href = this.el.getAttribute('href'); // get the href attribute of the link
+//     console.log(href); // log the href attribute of the link
+//     const section = document.querySelector(href);
+//     section.scrollIntoView({ behavior: 'smooth' }); // get the section element
+//   });
+// });
+
+const nav = document.querySelector('.nav'); // get the nav element
+// add event listener to the nav links
+const navLinks = document.querySelector('.nav__links'); // get the nav element
+navLinks.addEventListener('click', function (e) {
+  e.preventDefault(); // Prevent default action of the link
+  console.log(e.target); // log the target element of the event
+  if (!e.target.classList.contains('nav__link')) return; // check if the target element is a link
+  const href = e.target.getAttribute('href'); // get the href attribute of the link
+  const section = document.querySelector(href); // get the section element
+  section.scrollIntoView({ behavior: 'smooth' }); // scroll to the section smoothly
+}); // add event listener to the nav element
+
+//Tabbed component
+const operations_tabs = document.querySelector('.operations__tab-container'); // get the operations element
+const operations_content = document.querySelectorAll('.operations__content'); // get the operations content element
+const showContent = function (data_tab) {
+  operations_content.forEach(c => {
+    c.classList.remove('operations__content--active');
+  }); // get the child element count of the operations content element
+  const activeContent = document.querySelector(
+    `.operations__content--${data_tab}`
+  ); // get the active content element
+  activeContent.classList.add('operations__content--active'); // add the active class to the active content element
+};
+
+operations_tabs.addEventListener('click', function (e) {
+  e.preventDefault(); // Prevent default action of the link
+  const clicked = e.target.closest('.operations__tab'); // get the closest element with the class operations__tab
+  if (!clicked) return; // check if the clicked element is a tab
+  console.log(clicked); // log the clicked element
+  operations_tabs.querySelectorAll('.operations__tab').forEach(t => {
+    t.classList.remove('operations__tab--active'); // remove the active class from all other tabs
+  });
+  clicked.classList.add('operations__tab--active'); // add the active class to the clicked element
+
+  console.log(clicked.dataset); // log the dataset of the clicked element
+  console.log(clicked.dataset.tab); // log the tab of the clicked element
+  showContent(clicked.dataset.tab); // show the content of the tab
+  // log the target element of the event
+});
+
+btnScollTo.addEventListener('click', function (e) {
+  e.preventDefault(); // Prevent default action of the button
+  const s1coords = section1.getBoundingClientRect(); // get the coordinates of the section
+  // console.log(s1coords); // log the coordinates of the section
+  // console.log(e.target.getBoundingClientRect()); // log the coordinates of the button
+  // console.log('Current scroll (X/Y)', window.scrollX, window.scrollY); // log the current scroll position
+  // console.log(
+  //   'height/width of viewport',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // ); // log the height and width of the viewport
+  // Scrolling smoothly to the section
+  // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset); // scroll to the section
+  //old walll
+  //
+  // modern way to scroll to the section
+  section1.scrollIntoView({ behavior: 'smooth' }); // scroll to the section smoothly
+});
+
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target; // get the target element of the event
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link'); // get the closest element with the class nav
+    const logo = link.closest('.nav').querySelector('img'); // get the logo element
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this; // set the opacity of the siblings to 0.5
+    });
+    logo.style.opacity = this; // set the opacity of the logo to 0.5
+  }
+};
+
+navLinks.addEventListener('mouseover', handleHover.bind(0.5));
+navLinks.addEventListener('mouseout', handleHover.bind(1)); // set the opacity of the logo to 1
+
+// const initialCoords = section1.getBoundingClientRect();
+// console.log(initialCoords); // get the coordinates of the section
+// // sticky navigation
+// window.addEventListener('scroll', function (e) {
+//   const scrollY = window.scrollY;
+//   if (scrollY > initialCoords.top) {
+//     nav.classList.add('sticky'); // add the sticky class to the header
+//   } else {
+//     nav.classList.remove('sticky'); // remove the sticky class from the header
+//   }
+//   // get the current scroll position
+//   console.log(scrollY); // log the current scroll position
+// }); // add event listener to the window element
+
+// Intersection Observer API
+const stickyNav = function (entries, observer) {
+  const [entry] = entries; // get the first entry of the entries array
+  //console.log(entry); // log the entry
+  if (!entry.isIntersecting) {
+    nav.classList.add('sticky'); // add the sticky class to the header
+  } else {
+    nav.classList.remove('sticky'); // remove the sticky class from the header
+  }
+};
+
+const navHeight = nav.getBoundingClientRect().height; // get the height of the nav element
+
+const obsOptions = {
+  root: null, // use the viewport as the root element
+  threshold: 1, // when 0% or 20% of the target is visible
+  rootMargin: navHeight, // set the root margin to 0px
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0, // when 0% of the target is visible
+  rootMargin: `-${navHeight}px`, // set the root margin to the height of the nav element
+}); // create a new intersection observer
+
+headerObserver.observe(header); // observe the section element
+
+// reveal sections on scroll
+
+const allSections = document.querySelectorAll('.section'); // get all the sections
+const revealSection = function (entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return; // check if the entry is not intersecting
+    entry.target.classList.remove('section--hidden'); // remove the hidden class from the section element
+    observer.unobserve(entry.target);
+  });
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null, // use the viewport as the root element
+  threshold: 0.15, // when 15% of the target is visible
+}); // create a new intersection observer
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section); // observe the section element
+  section.classList.add('section--hidden'); // add the hidden class to the section element
+}); //observe the section element
+
+// lazy loading images
+
+const allFeatureImages = document.querySelectorAll('.features__img');
+
+const loadImg = function (entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return; // check if the entry is not intersecting
+    //dataset  data-src is used to get the data-src attribute of the image element
+    entry.target.src = entry.target.dataset.src; // set the src attribute of the image element to the data-src attribute
+    entry.target.addEventListener('load', function () {
+      entry.target.classList.remove('lazy-img'); // remove the lazy-img class from the image element
+    });
+    observer.unobserve(entry.target); // unobserve the image element
+  });
+};
+
+const featureObserver = new IntersectionObserver(loadImg, {
+  root: null, // use the viewport as the root element
+  threshold: 0, // when 0% of the target is visible
+  rootMargin: '200px', // set the root margin to 200px
+});
+
+allFeatureImages.forEach(function (img) {
+  featureObserver.observe(img); // observe the image element
+}); // observe the image element
+
+const silder = document.querySelector('.slider'); // get the slider element
+// translate slider
+const sliderbtnleft = document.querySelector('.slider__btn--left'); // get all the slides
+const sliderbtnright = document.querySelector('.slider__btn--right'); // get all the slides
+const allSlide = document.querySelectorAll('.slide'); // get the slider element
+
+let currentSlide = 0; // set the current slide to 0
+const maxSlide = allSlide.length;
+
+//dot
+const dotContainer = document.querySelector('.dots'); // get the dots element
+
+const createDots = function () {
+  allSlide.forEach((_, i) => {
+    dotContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    ); // create a new button element and insert it into the dots element
+  });
+};
+const updateDot = function (silder) {
+  const dots = document.querySelectorAll('.dots__dot'); // get all the dots
+  dots.forEach(dot => dot.classList.remove('dots__dot--active')); // remove the active class from all other dots
+  dots[silder].classList.add('dots__dot--active'); // add the active class to the clicked element
+};
+createDots();
+goToSlide(0);
+updateDot(0); // set the active dot to the first dot
+const MoveSlide = function () {
+  if (this === 1) {
+    if (currentSlide === maxSlide - 1) {
+      currentSlide = 0; // set the current slide to 0
+    } else {
+      currentSlide += this; // increment the current slide by 1
+    }
+  } else if (this === -1) {
+    if (currentSlide === 0) {
+      currentSlide = maxSlide - 1; // set the current slide to the last slide
+    } else {
+      currentSlide += this; // decrement the current slide by 1
+    }
+  }
+  updateDot(currentSlide); // set the active dot
+  goToSlide(currentSlide);
+};
+
+sliderbtnleft.addEventListener('click', MoveSlide.bind(1)); // add event listener to the left button
+
+sliderbtnright.addEventListener('click', MoveSlide.bind(-1));
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowRight') {
+    MoveSlide.call(1); // call the MoveSlide function with the value of 1
+  } else if (e.key === 'ArrowLeft') {
+    MoveSlide.call(-1); // call the MoveSlide function with the value of -1
+  }
+}); // add event listener to the document element
+
+function goToSlide(slide) {
+  allSlide.forEach((s, i) => {
+    s.style.transform = `translateX(${100 * (i - slide)}%) `;
+  });
+}
+
+dotContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    const { slide } = e.target.dataset; // get the dataset of the target element
+    console.log(slide); // log the slide number
+    updateDot(slide); // set the active dot
+    goToSlide(slide); // go to the slide
+  }
+});
+///test
+
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.innerHTML =
+//   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// console.log(header); // log the message element
+// header.prepend(message); // prepend to the header
+// header.append(message);
+
+// remove element after 3 seconds
+// setTimeout(() => {
+//   message.remove();
+// }, 3000);
+
+//styles
+
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+
+// console.log(getComputedStyle(message).height); // get the height of the message element
+
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height) + 40 + 'px'; // get the height of the message element and add 30px to it
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered'); // set the primary color to orangered
+
+// //attribute
+
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt); // log the alt attribute of the logo element
+// console.log(logo.src); // log the src attribute of the logo element
+// console.log(logo.className);
